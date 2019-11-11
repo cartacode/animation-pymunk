7669c1f51f3448622770fbac422920d2e73b26f7
it-avenger <simbawow8@gmail.com>
2019-11-11 13:29:39 -0500

    revise udpated

--- plc.py
+++ plc.py
@@ -22,8 +22,6 @@ import pygame
 from pygame.locals import *
 from pygame.color import *
 import pymunk
-import pymunk.pygame_util
-from pymunk import Vec2d
 
 #########################################
 # inet conf
@@ -62,11 +60,9 @@ PLC_TAG_MOTOR = 0x3
 PLC_TAG_NOZZLE = 0x4
 PLC_TAG_RUN = 0x10
 
-
 # Global Variables
 global bottles
 bottles = []
-
 
 def to_pygame(p):
     """Small hack to convert pymunk to pygame coordinates"""
@@ -79,9 +75,10 @@ def add_ball(space):
     radius = 3
     inertia = pymunk.moment_for_circle(mass, 0, radius, (0,0))
     body = pymunk.Body(mass, inertia)
-
+    # body._bodycontents.v_limit = 120
+    # body._bodycontents.h_limit = 1
     x = random.randint(181,182)
-    body.position = x, 400
+    body.position = x, 410
     shape = pymunk.Circle(body, radius, (0,0))
     shape.collision_type = 0x5 #liquid
     space.add(body, shape)
@@ -121,34 +118,24 @@ def add_limit_switch(space):
 def add_nozzle(space):
     body = pymunk.Body()
     body.position = (180, 430)
-    # shape = pymunk.Poly.create_box(body, (15, 20), (0, 0), 0)
-
-    vs = [(170, 400),(170, 430),(190, 400),(190, 430)]
-    # vs = [(160, 400),(160, 430),(180, 400),(180, 430)]
-    shape = pymunk.Poly(body, vs)
+    shape = pymunk.Poly.create_box(body, (15, 20), (0, 0), 0)
     space.add(shape)
     return shape
 
 def add_base(space):
-    body = pymunk.Body(body_type=pymunk.Body.STATIC)
+    body = pymunk.Body()
     body.position = (0, 300)
-    # shape = pymunk.Poly.create_box(body, (SCREEN_WIDTH, 20), ((SCREEN_WIDTH/2), -10), 0)
-    
-    vs = [(0, 260),(0, 300),(SCREEN_WIDTH, 260),(SCREEN_WIDTH, 300)]
-    shape = pymunk.Poly(body, vs)
-
+    shape = pymunk.Poly.create_box(body, (SCREEN_WIDTH, 20), (0, 0), 0)
     shape.friction = 1.0
     shape.collision_type = 0x6 # base
-    space.add(body,shape)
+    space.add(shape)
     return shape
 
 def add_bottle(space):
     mass = 10
     inertia = 0xFFFFFFFFF #7*16
-    # body = pymunk.Body(mass, inertia)
-    body = pymunk.Body(mass, inertia, pymunk.Body.KINEMATIC)
-    body.position = (130,301)
-    body.collision_type = 0x15
+    body = pymunk.Body(mass, inertia)
+    body.position = (130,300)
     l1 = pymunk.Segment(body, (-150, 0), (-100, 0), 2.0)
     l2 = pymunk.Segment(body, (-150, 0), (-150, 100), 2.0)
     l3 = pymunk.Segment(body, (-100, 0), (-100, 100), 2.0)
@@ -163,8 +150,8 @@ def add_bottle(space):
     l2.collision_type = 0x3 # bottle_side
     l3.collision_type = 0x3 # bottle_side
 
-    space.add(l1,l2,l3)
-    return l1,l2,l3, body
+    space.add(l1, l2, l3, body)
+    return l1,l2,l3
 
 def draw_polygon(screen, shape):
     points = shape.get_vertices()
@@ -175,7 +162,7 @@ def draw_polygon(screen, shape):
 
 
 def draw_lines(screen, lines, color=THECOLORS['dodgerblue4']):
-    for line in lines[:3]:
+    for line in lines:
         body = line.body
         pv1 = body.position + line.a.rotated(body.angle)
         pv2 = body.position + line.b.rotated(body.angle)
@@ -194,15 +181,15 @@ def level_ok(space, arbiter, *args, **kwargs):
     PLCSetTag(PLC_TAG_NOZZLE, 0) # Close nozzle
     return False
 
-def bottle_in_place(arbiter, space, *args, **kwargs):
+def bottle_in_place(space, arbiter, *args, **kwargs):
     log.debug("Bottle in place")
     PLCSetTag(PLC_TAG_LIMIT_SWITCH, 1)
     PLCSetTag(PLC_TAG_LEVEL_SENSOR, 0)
     PLCSetTag(PLC_TAG_NOZZLE, 1) # Open nozzle
-
     return False
 
-def add_new_bottle(arbiter, space, *args, **kwargs):
+def add_new_bottle(space, arbiter, *args, **kwargs):
+    global bottles
     bottles.append(add_bottle(space))
     log.debug("Adding new bottle")
     return False
@@ -220,25 +207,21 @@ def runWorld():
     space = pymunk.Space()
     space.gravity = (0.0, -900.0)
 
-    # # Limit switch with bottle bottom
-    space.add_collision_handler(0x1, 0x2).begin=bottle_in_place
-    # # Level sensor with water
-    space.add_collision_handler(0x4, 0x5).begin=level_ok
-
+    # Limit switch with bottle bottom
+    space.add_collision_handler(0x1, 0x2, begin=bottle_in_place)
+    # Level sensor with water
+    space.add_collision_handler(0x4, 0x5, begin=level_ok)
     # Level sensor with ground
-    space.add_collision_handler(0x4, 0x6).begin=no_collision
+    space.add_collision_handler(0x4, 0x6, begin=no_collision)
     # Limit switch with ground
-    space.add_collision_handler(0x1, 0x6).begin=no_collision
+    space.add_collision_handler(0x1, 0x6, begin=no_collision)
     # Limit switch with bottle side
-    space.add_collision_handler(0x1, 0x3).begin=no_collision
+    space.add_collision_handler(0x1, 0x3, begin=no_collision)
     # Level sensor with bottle side
-    space.add_collision_handler(0x4, 0x3).begin=no_collision
+    space.add_collision_handler(0x4, 0x3, begin=no_collision)
     # Bottle in with bottle sides and bottom
-    space.add_collision_handler(0x7, 0x2).begin=no_collision
-    space.add_collision_handler(0x7, 0x2).separate=add_new_bottle
-
-    space.add_collision_handler(0x7, 0x3).begin=no_collision
-
+    space.add_collision_handler(0x7, 0x2, begin=no_collision, separate=add_new_bottle)
+    space.add_collision_handler(0x7, 0x3, begin=no_collision)
 
     base = add_base(space)
     nozzle = add_nozzle(space)
@@ -271,32 +254,27 @@ def runWorld():
         screen.blit(foo, ( 0,0))
 
         if PLCGetTag(PLC_TAG_RUN): # if the plant working
-            # Motor Logic
-            if (PLCGetTag(PLC_TAG_LIMIT_SWITCH) == 1):
-                PLCSetTag(PLC_TAG_MOTOR, 0)
+                # Motor Logic
+                if (PLCGetTag(PLC_TAG_LIMIT_SWITCH) == 1):
+                    PLCSetTag(PLC_TAG_MOTOR, 0)
 
-            if (PLCGetTag(PLC_TAG_LEVEL_SENSOR) == 1):
-                PLCSetTag(PLC_TAG_MOTOR, 1)
+                if (PLCGetTag(PLC_TAG_LEVEL_SENSOR) == 1):
+                    PLCSetTag(PLC_TAG_MOTOR, 1)
 
-            ticks_to_next_ball -= 1
+                ticks_to_next_ball -= 1
 
-            if not PLCGetTag(PLC_TAG_LIMIT_SWITCH):
-                PLCSetTag(PLC_TAG_MOTOR, 1)
+                if not PLCGetTag(PLC_TAG_LIMIT_SWITCH):
+                    PLCSetTag(PLC_TAG_MOTOR, 1)
 
-            if ticks_to_next_ball <= 0 and PLCGetTag(PLC_TAG_NOZZLE):
-                ticks_to_next_ball = 1
-                ball_shape = add_ball(space)
-                balls.append(ball_shape)
+                if ticks_to_next_ball <= 0 and PLCGetTag(PLC_TAG_NOZZLE):
+                    ticks_to_next_ball = 1
+                    ball_shape = add_ball(space)
+                    balls.append(ball_shape)
 
-            # Move the bottles
-            if PLCGetTag(PLC_TAG_MOTOR) == 1:
-                for bottle in bottles:
-                    # bottle[0].body.position.x += 0.25
-                    # bottle[0].body.position = Vec2d(bottle[0].body.position.x+1,
-                    #                                 bottle[0].body.position.y)
-
-                    bottle[3].position = Vec2d(bottle[3].position.x+0.25,
-                                                bottle[3].position.y)
+                # Move the bottles
+                if PLCGetTag(PLC_TAG_MOTOR) == 1:
+                    for bottle in bottles:
+                        bottle[0].body.position.x += 0.25
 
         # Draw water balls
         # Remove off-screen balls
@@ -314,7 +292,7 @@ def runWorld():
         # Draw bottles
         for bottle in bottles:
             if bottle[0].body.position.x > SCREEN_WIDTH+150 or bottle[0].body.position.y < 150:
-                # space.remove(bottle, bottle[0].body)
+                space.remove(bottle, bottle[0].body)
                 bottles.remove(bottle)
                 continue
             draw_lines(screen, bottle)
@@ -334,7 +312,7 @@ def runWorld():
         screen.blit(name, (10, 10))
         screen.blit(instructions, (SCREEN_WIDTH-115, 10))
 
-        space.step(1.0/FPS)
+        space.step(1/FPS)
         pygame.display.flip()
 
 
